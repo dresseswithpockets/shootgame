66a059352c0cd3a90111263c82119ca957ec9f1a
Ashley <dresswithpockets@pm.me>
2024-04-25 14:04:21 -0700

    remove unused code

--- src/entity.c
+++ src/entity.c
@@ -20,6 +20,7 @@ void draw_ent(Entity* entity) {
 void draw_ent_debug(Entity* entity) {
     DrawCircleV(entity->pos, 1.0, RED);
     // aabb rect
+
     Rectangle aabb = {
         entity->pos.x - entity->c_size.x / 2.0,
         entity->pos.y - entity->c_size.y / 2.0,
@@ -27,12 +28,8 @@ void draw_ent_debug(Entity* entity) {
         entity->c_size.y,
     };
     DrawRectangleLinesEx(aabb, 1.0f, YELLOW);
-    // Rectangle aabb = entity->c_aabb;
-    // aabb.x += pixel_pos.x - aabb.width / 2.0;
-    // aabb.y += pixel_pos.y - aabb.height / 2.0;
-    // DrawRectangleLinesEx(aabb, 1.0, YELLOW);
+
     // radius
-    //Vector2 radial_center = Vector2Add(Vector2Scale(entity->c_radial_center, CELL_SIZE), pixel_pos);
     DrawCircleLinesV(get_world_radial_center(entity), entity->c_radius, BLUE);
 }
 
@@ -114,50 +111,4 @@ void ent_move(GameState* state, Entity* entity) {
     }
 
     entity->pos = Vector2Add(v2itof(entity->pos_pixel), entity->pos_subpixel);
-
-    // int horizontal_cell_size = (int)(entity->c_aabb.width / 2.0 / CELL_SIZE);
-    // float horizontal_cell_remainder = 1.0 - fmodf(entity->c_aabb.width / 2.0, (float)CELL_SIZE);
-
-    // if (tile_has_collision(entity->cpos.x + entity->c_size.x, entity->cpos.y) && entity->fpos.x >= 0.8) {
-    //     entity->fpos.x = 0.8;
-    //     entity->velocity.x = 0.0;
-    // }
-    // if (tile_has_collision(entity->cpos.x - 1, entity->cpos.y) && entity->fpos.x <= 0.3) {
-    //     entity->fpos.x = 0.3;
-    //     entity->velocity.x = 0.0;
-    // }
-
-    // while (entity->fpos.x > 1) {
-    //     entity->cpos.x += 1;
-    //     entity->fpos.x -= 1;
-    // }
-    // while (entity->fpos.x < 0) {
-    //     entity->cpos.x -= 1;
-    //     entity->fpos.x += 1;
-    // }
-
-    // // vertical movement
-    // entity->fpos.y += entity->velocity.y * state->game_data->dt;
-    // entity->velocity.y *= entity->normal_friction;
-
-    // // HACK: tile collision here is kinda funky - it assumes that the player is 1x1 cells big
-    // //       I have a suspicion it might be better to just store the subpixel position of the player
-    // //       and to map it to tile coords when needed
-    // if (tile_has_collision(entity->cpos.x, entity->cpos.y + entity->c_size.y) && entity->fpos.y >= 0.7) {
-    //     entity->fpos.y = 0.7;
-    //     entity->velocity.y = 0.0;
-    // }
-    // if (tile_has_collision(entity->cpos.x, entity->cpos.y - 1) && entity->fpos.y <= 0.3) {
-    //     entity->fpos.y = 0.3;
-    //     entity->velocity.y = 0.0;
-    // }
-
-    // while (entity->fpos.y > 1) {
-    //     entity->cpos.y += 1;
-    //     entity->fpos.y -= 1;
-    // }
-    // while (entity->fpos.y < 0) {
-    //     entity->cpos.y -= 1;
-    //     entity->fpos.y += 1;
-    // }
 }
