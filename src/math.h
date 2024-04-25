e81c3d98c1fa469ff43ba97f177fe4ede0921a3f
Ashley <dresswithpockets@pm.me>
2024-04-25 13:29:15 -0700

    add some vector2i utility functions

--- src/math.h
+++ src/math.h
@@ -12,6 +12,18 @@ typedef struct Vector2i {
 
 #define ARRAY_LEN(arr) (sizeof(arr)/sizeof(arr[0]))
 
+static inline Vector2 v2itof(Vector2i vector) {
+    return (Vector2) {(float)vector.x, (float)vector.y};
+}
+
+static inline Vector2i v2ftoi(Vector2 vector) {
+    return (Vector2i) {(int)vector.x, (int)vector.y};
+}
+
+static inline Vector2i vector2i_lerp(Vector2i a, Vector2i b, float t) {
+    return v2ftoi(Vector2Lerp(v2itof(a), v2itof(b), t));
+}
+
 static inline float rand_float(float min, float max) {
     return ((float)GetRandomValue(0, INT_MAX)/(float)(INT_MAX)) * (max - min) + min;
 }
