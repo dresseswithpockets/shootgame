d2539819adcd79f5a49cd4f721297d051b21cd5f
Ashley <dresswithpockets@pm.me>
2024-05-19 16:52:55 -0700

    fix linear bullet directional sprite

--- src/bullet/bullet.gd
+++ src/bullet/bullet.gd
@@ -4,6 +4,9 @@ class_name Bullet
 enum { TEAM_PLAYER, TEAM_ENEMY }
 
 @onready var area: Area2D = $Area2D
+@onready var sprite_north: Sprite2D = $SpriteNorth
+@onready var sprite_west: Sprite2D = $SpriteWest
+@onready var sprite_north_west: Sprite2D = $SpriteNorthWest
 
 @export var speed: float = 80
 @export var destroy_on_hit: bool = true
@@ -15,6 +18,19 @@ enum { TEAM_PLAYER, TEAM_ENEMY }
 
 var direction: Vector2 = Vector2.UP
 
+# sprite, flip_h, flip_v; LUT for each 8 directions
+@onready var _sprite_data = [
+    [sprite_west, true, false],
+    [sprite_north_west, true, true],
+    [sprite_north, false, true],
+    [sprite_north_west, false, true],
+    [sprite_west, false, false],
+    [sprite_north_west, false, false],
+    [sprite_north, false, false],
+    [sprite_north_west, true, false],
+    [sprite_west, true, false],
+]
+
 func _ready() -> void:
     area.area_entered.connect(_on_area_entered)
     match team:
@@ -22,6 +38,17 @@ func _ready() -> void:
             area.collision_mask = team_player_mask
         TEAM_ENEMY:
             area.collision_mask = team_enemy_mask
+
+    var direction_angle := fmod(direction.angle(), TAU) / TAU
+    # angles returned by angle() are between [-PI, PI), with the center(0) at Vector2.RIGHT
+    if direction_angle < 0:
+        direction_angle = 1.0 + direction_angle
+    direction_angle = floori((direction_angle * 8) + 0.5) # [0, 8]
+    var sprite_info = _sprite_data[direction_angle]
+    var sprite: Sprite2D = sprite_info[0]
+    sprite.visible = true
+    sprite.flip_h = sprite_info[1]
+    sprite.flip_v = sprite_info[2]
 
 func _physics_process(delta: float) -> void:
     position += direction * speed * delta
@@ -38,3 +65,4 @@ func _on_area_entered(area: Area2D) -> void:
     # TODO: how should it work when the area is a bullet?
     if destroy_on_hit:
         queue_free()
+
