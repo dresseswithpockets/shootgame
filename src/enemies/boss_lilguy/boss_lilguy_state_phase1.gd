b603362af2f62ce5c9c9bb5635c8ec4f596cfa63
Ashley <dresswithpockets@pm.me>
2024-06-28 16:35:18 -0700

    add spreadshots to phase1 of the boss

--- src/enemies/boss_lilguy/boss_lilguy_state_phase1.gd
+++ src/enemies/boss_lilguy/boss_lilguy_state_phase1.gd
@@ -15,12 +15,21 @@ class_name BossLilguyStatePhase1 extends BossLilguyState
 @export var multishot_cluster_size: int = 3
 @export var shoot_delay: float = 1.5
 
+@export_subgroup("Spreadshot")
+@export var spreadshot_prefab: PackedScene = preload("res://bullet/linear_bullet.tscn")
+@export var spreadshot_linear_speed: float = 60
+@export var spreadshot_chance: float = 0.2
+@export var spreadshot_count: int = 7
+@export var spreadshot_fire_delay: float = 0.06666666666666666666666666666667
+
 var last_x: float = 0
 @onready var next_x: float = randf_range(bound_left, bound_right)
 @onready var move_time: float = floori(randf_range(move_time_min, move_time_max))
 @onready var move_timer: float = move_time
 
 @onready var shoot_timer: float = shoot_delay
+@onready var spreadshot_fire_timer: float = spreadshot_fire_delay
+@onready var spreadshot_pending_count: int = 0
 
 func ready() -> void:
     last_x = boss_lilguy.position.x
@@ -47,10 +56,22 @@ func enter(msg) -> void:
     pass
 
 func try_shoot(delta: float) -> void:
-    shoot_timer -= delta
-    if shoot_timer <= 0:
-        shoot_baseshot()
-        shoot_timer = shoot_delay
+    if spreadshot_pending_count == 0:
+        shoot_timer -= delta
+        if shoot_timer <= 0:
+            if randf() <= spreadshot_chance:
+                spreadshot_pending_count = spreadshot_count
+            else:
+                shoot_baseshot()
+            shoot_timer = shoot_delay
+
+    if spreadshot_pending_count > 0:
+        spreadshot_fire_timer -= delta
+        if spreadshot_fire_timer <= 0:
+            shoot_spreadshot()
+            spreadshot_fire_timer = spreadshot_fire_delay
+            spreadshot_pending_count -= 1
+
 
 # TODO: maybe i should have a cluster "wrangler" which automatically sets
 #       angular_radius and angular_offset based on the number of bullets in the
@@ -65,6 +86,21 @@ func shoot_cluster(bullet_prefab: PackedScene, cluster_size: int, position: Vect
         bullet.angular_offset = TAU * cluster_idx / cluster_size
         bullet.direction = direction
         Global.game.add_bullet(bullet)
+
+func shoot_linear(bullet_prefab: PackedScene, position: Vector2, direction: Vector2, linear_speed: float, team: int):
+    var bullet: BulletLinear = bullet_prefab.instantiate()
+    bullet.global_position = position
+    bullet.linear_speed = linear_speed
+    bullet.team = team
+    bullet.direction = direction
+    Global.game.add_bullet(bullet)
+
+func shoot_spreadshot() -> void:
+    var t := (float(spreadshot_pending_count) / float(spreadshot_count))
+    var right_angle := lerp_angle(0, deg_to_rad(70), 1 - t)
+    var left_angle := lerp_angle(deg_to_rad(110), PI, t)
+    shoot_linear(spreadshot_prefab, boss_lilguy.global_position, Vector2.from_angle(right_angle), spreadshot_linear_speed, Bullet.TEAM_ENEMY)
+    shoot_linear(spreadshot_prefab, boss_lilguy.global_position, Vector2.from_angle(left_angle), spreadshot_linear_speed, Bullet.TEAM_ENEMY)
 
 func shoot_baseshot() -> void:
     var direction := boss_lilguy.global_position.direction_to(Global.player.global_position)
